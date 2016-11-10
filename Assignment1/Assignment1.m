8bc45321b6ad3387cc4461a02f9b6bba5c952f7c
deZakelijke <git@michadegroot.nl>
2016-11-10 21:04:22 +0100

    assignment 1 seems to work

--- Assignment1/Assignment1.m
+++ Assignment1/Assignment1.m
@@ -7,38 +7,37 @@ function Assignment1()
 % Robot 03
 %     henk = legoev3('wifi','10.42.0.131', '0016534397ff');
 % Robot 05
-     henk = legoev3('wifi','10.42.0.131', '00165344ec79');
-    
+%     henk = legoev3('wifi','10.42.0.131', '00165344ec79');
+% Robot 012
+    henk = legoev3('wifi','10.42.0.106', '0016534513f5');
     beep(henk)
     
-    mainSpeed = 40;         % some unit, actually power to wheel, used as ground speed
+    mainSpeed = 40.0;         % some unit, actually power to wheel, used as ground speed
     timeInterval = 0.1;     % in seconds
-    runtime = 10;            % in seconds. Actual runtime is rt/ti
-    %angle = 0;              % in radials, ICC
+    runtime = 5;            % in seconds. Actual runtime is rt/ti
+    orientation = [0,0,0];   % in cm and radials, location and angle with the origin of the initial frame
     axisLength = 12.5;      % in centimeters, approximation
-    %wheelRadius = 2.75;     % in centimeters, approximation
-    ICC_Distance = -20;      % in centimeters
-    straight = 12500000        % ICC_D for straight
+    wheelRadius = 2.75;     % in centimeters, approximation
+    ICC_Distance = -20.0;     % in centimeters
+    straight = 12500000.0;    % ICC_D for straight
     
     motorRight = motor(henk, 'C');
     motorLeft = motor(henk, 'B');
     
-    iterations = runtime/timeInterval
+    iterations = runtime/timeInterval;
     for i = 1: iterations
         if i < iterations * (1 / 4)
-            ICC_Distance = straight
-        elseif i >= iterations * (1 / 4) & i < iterations * (3 / 8)
-            ICC_Distance = 20
-        elseif i >= iterations * (3 / 8) & i < iterations * (1 / 2)
-            ICC_Distance = straight
-        elseif i >= iterations * (1 / 2) & i < iterations * (5 / 8)
-            ICC_Distance = -20
+            ICC_Distance = straight;
+        elseif i >= iterations * (1 / 4) && i < iterations * (3 / 8)
+            ICC_Distance = 20;
+        elseif i >= iterations * (3 / 8) && i < iterations * (1 / 2)
+            ICC_Distance = straight;
+        elseif i >= iterations * (1 / 2) && i < iterations * (5 / 8)
+            ICC_Distance = -20;
         elseif i >=iterations * (5 / 8)
-            ICC_Distance = straight
+            ICC_Distance = straight;
         end
             
-        
-        
         [motorRightSpeed, motorLeftSpeed] = calculateSpeed(mainSpeed, axisLength, ICC_Distance);
 
         motorRight.Speed = motorRightSpeed;
@@ -46,19 +45,46 @@ function Assignment1()
 
         start(motorRight)
         start(motorLeft)
-        pause(timeInterval);
+        pause(timeInterval); 
         
+        distanceRight = double(readRotation(motorRight))*wheelRadius;
+        distanceLeft = double(readRotation(motorLeft))*wheelRadius;
+        
+        if distanceRight == distanceLeft
+            %angularRotation = 0;
+            %iterationDistance = distanceRight;
+            newRobotOrientation = [distanceRight,0.0,0.0];
+        else
+            angularRotation = double(calculateRotation(distanceRight, distanceLeft, axisLength));
+            %iterationDistance = angularRotation * ICC_Distance;
+            newRobotOrientation = [ cos(angularRotation) *ICC_Distance, sin(angularRotation)* ICC_Distance, angularRotation];
+        end
+        angle = double(orientation(3));
+        rotationMatrix = [[cos(angle), sin(angle),0]; [-sin(angle), cos(angle),0]; [0,0,1]];
+        
+        newInitialOrientation = newRobotOrientation * rotationMatrix;
+        orientation = orientation + newInitialOrientation;
     end
+    
     stop(motorRight)
     stop(motorLeft)
-    
+    orientation
     
 end
 
+% calculates the power for the wheels based on the speed and the circle the
+% robot is driving on
 function [speedRight, speedLeft] = calculateSpeed(mainSpeed, axisLength, ICC_Distance)
     omega = mainSpeed/ICC_Distance;
     
     speedRight = omega*(ICC_Distance + axisLength/2);
     speedLeft = omega*(ICC_Distance - axisLength/2);
 
+end
+
+% Calculates the change in angle compared to the previous
+% iteration/timeInterval
+% This depends on how we have defined our initial system
+function [angularRotation] = calculateRotation(distanceRight, distanceLeft, axisLength)
+    angularRotation = (distanceRight - distanceLeft)/ axisLength;
 end
