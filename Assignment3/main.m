57be1011a090d08c37b32bbd6aa9b1215b44e19b
Yuri_Sturkenboom <y.a.sturkenboom@gmail.com>
2016-11-27 23:28:47 +0100

    Made report, cleaned up code of main.m

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -2,6 +2,8 @@
 %
 %   Author Davide Scaramuzza - davide.scaramuzza@ieee.org
 %   ETH Zurich - April, 25, 2007
+%   Edited by Micha de Groot, Harm Manders and Yuri Sturkenboom
+%   University of Amsterdam - November 27, 2016
 
 clc;
 url = get_camera_url();
@@ -13,9 +15,9 @@ url = get_camera_url();
 %                     Rmax was already loaded when calling "calibrate_camera.m"
 % Rmin = 77;%         Min detectable distance in pixels in VGA image
 %                     Rmin was already loaded when calling "calibrate_camera.m"
-alpha = 130;%          Radial distortion coefficient, YOU MAY NEED TO TUNE THIS PARAMETER!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-height = 0.10;%       camera height in meters, YOU MAY NEED TO TUNE THIS PARAMETER!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-Bwthreshold = 100;%   Threshold for segment the image into Black & white colors, YOU MAY NEED TO TUNE THIS PARAMETER!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+alpha = 130;%          Radial distortion coefficient
+height = 0.10;%       camera height in meters
+Bwthreshold = 100;%   Threshold for segment the image into Black & white colors
 angstep = 1.0;%       Angular step of the beam in degrees
 axislimit = 0.2;%     Axis limit
 
@@ -24,28 +26,23 @@ axislimit = 0.2;%     Axis limit
 % MAIN
 % -------------------------------------------------------------------------
 
-%[Rmin, Rmax, center] = calibrate_camera();
-Rmin = 60;
-Rmax = 160;
-center = [324.9906 258.7876];
+% The camera's parameters are found by manual calibration
+[Rmin, Rmax, center] = calibrate_camera();
+
 %while 1
     tic;%                               Start counting elapsed time
     
-    %snapshot = imread(url); %           acquire image    
+    % an image is acquired
     snapshot = imread('example_a.jpg');
 
+    % here the image is undistorted
     [undistortedimage, theta] = imunwrap(snapshot, center, angstep, Rmax, Rmin);
 
-    figure;
-    imagesc(undistortedimage);
-    colormap(gray);
-    %imshow(undistortedimage);
-    
-    % parameter needs fintetuning
+    % the image is then thresholded to find obstacles
     BWimg = img2bw(undistortedimage, Bwthreshold); 
-    rho = getpixeldistance(BWimg, Rmin)
-    %imshow(BWimg, [0 1]);
 
+    % returns a *vecotr* of distances for each of rho
+    rho = getpixeldistance(BWimg, Rmin)
 
     figure;
     imagesc(snapshot);
@@ -53,22 +50,17 @@ center = [324.9906 258.7876];
     drawlaserbeam(center, theta, rho);
     hold off;
 
-
+    % here the distance measurements are undistorted and plotted
     dist = undistort_dist_points(theta, rho, alpha, height);
     figure;
     draw_undistorted_beam(dist, theta, axislimit);
 
-
     sigmaRho = compute_stdDev(rho)
 
+    % finally, the uncertainty for each distance measure is computed and plotted
     sigmaDist = compute_uncertainty(rho, sigmaRho, alpha, height);
     hold on;
     draw_uncertainty(dist, theta, sigmaDist);
-
-    % READ THE PDF DOCUMENT AND FILL THIS LOOP 
-    % REMEMBER THAT YOU HAVE TO FLIP THE IMAGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-    
     
     
 % Compute the time per frame and effective frame rate.
